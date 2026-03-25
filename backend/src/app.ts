0d0fbc172b1826f6107924ab47cd22635d0695b7
SyncWithRaj <rajr127655@gmail.com>
2026-03-25 14:55:58 +0530

    fixed frontend url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // your frontend
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", // your frontend
     credentials: true,
   })
 );
